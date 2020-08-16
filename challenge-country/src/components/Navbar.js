a34087a8b8317c066a5b6870d5ba16e27c12f1a1
Adesola Oni-Shogbonyo <s.oni-shogbonyo@hotmail.co.uk>
2020-08-16 17:32:27 +0100

    impleemting xstate

--- challenge-country/src/components/Navbar.js
+++ challenge-country/src/components/Navbar.js
@@ -1,5 +1,20 @@
 import React from 'react'
 
+import { Machine } from "xstate"
+import { useMachine } from "@xstate/react"
+
+const changeTheme = Machine({
+  id: "theme",
+  initial: "dark",
+  states: {
+    dark: {
+      on: { CHANGE: "light" },
+    },
+    light: {
+      on: { CHANGE: "dark" },
+    },
+  },
+})
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -14,8 +29,11 @@ class Navbar extends React.Component {
   handleDarkMode(e) {
     this.setState({ clickTerm: !this.state.clickTerm })
   }
-  
+
+ 
+
   render() {
+    const [current, send] = useMachine(changeTheme)
     return (
       <div>
         {/* <nav class={` navbar navbar-expand-lg navbar-light ${this.state.clickTerm ? 'bg-dark' : 'bg-light '} `}>
